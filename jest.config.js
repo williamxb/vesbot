dd7b7cdcef7eacaeae713829a9ffbc6ea1ebdfa7
Will Knight <45063456+williamxb@users.noreply.github.com>
2025-10-16 13:14:02 +0100

    feat: add jest

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  testEnvironment: 'node',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/**/*.test.js'
+  ],
+  testMatch: ['**/__tests__/**/*.test.js']
+};
