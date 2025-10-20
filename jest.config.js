d7602b0e9f4a606631d204e0263061592f21c3b1
Will <45063456+williamxb@users.noreply.github.com>
2025-10-20 14:57:02 +0100

    ci: fix formatting

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  testEnvironment: "node",
-  coverageDirectory: "coverage",
-  collectCoverageFrom: ["commands/**/*.js", "helpers/*.js"],
-  testMatch: ["**/__tests__/**/*.test.js"],
-  reporters: ["default", "jest-junit"],
-  "jest-junit": {
-    outputDirectory: ".",
-    outputName: "junit.xml",
+  testEnvironment: 'node',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['commands/**/*.js', 'helpers/*.js'],
+  testMatch: ['**/__tests__/**/*.test.js'],
+  reporters: ['default',·'jest-junit'],
+  'jest-junit': {
+    outputDirectory: '.',
+    outputName: 'junit.xml',
   },
 };
