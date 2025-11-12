5343f8fb882775be616d84e2c371d658a175142e
Will <45063456+williamxb@users.noreply.github.com>
2025-11-12 14:15:55 +0000

    fix: retry

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,5 @@ module.exports = {
 	coverageDirectory: 'coverage',
 	collectCoverageFrom: ['commands/**/*.js', 'helpers/*.js'],
 	testMatch: ['**/__tests__/**/*.test.js'],
-	testURL: 'http://localhost',
 	reporters: ['default', 'jest-junit'],
 };
