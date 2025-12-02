9aa5b73f4f6d2590044a2b5a0fce302afdbc1e1a
Will Knight <45063456+williamxb@users.noreply.github.com>
2025-12-02 03:23:38 +0000

    fix: improve createVehicleYear

--- helpers/formatting/createVehicleYear.js
+++ helpers/formatting/createVehicleYear.js
@@ -2,11 +2,14 @@
  * Create vehicle year
  * @param {Object} entire response from all successful APIs
  * @returns {string} // year of vehicle
- * @TODO string or int?
  */
 function createVehicleYear(data) {
-	if (data?.mot?.manufactureDate) return data.mot.manufactureDate.split('-')[0];
-	return `${data.ves?.yearOfManufacture} ` || `${data.vin?.Year} ` || ''; // empty string if no year
+	let output = 'Unknown Year ';
+	if (data?.mot?.manufactureDate) output = data.mot.manufactureDate.split('-')[0] + ' ';
+	if (data?.mot?.manufactureYear) output = data.mot.manufactureYear + ' ';
+	if (data?.ves?.yearOfManufacture) output = data.ves.yearOfManufacture + ' ';
+	if (data?.vin?.plate_lookup?.year) output = data.vin.plate_lookup.year + ' ';
+	return { year: output };
 }
 
 module.exports = { createVehicleYear }
