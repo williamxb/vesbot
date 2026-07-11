c5ace3abd4f9aa94f874f3cfe7ab3af497f1f315
Will K <45063456+williamxb@users.noreply.github.com>
2026-07-11 22:07:03 +0100

    fix: add regex for NI plates

--- helpers/validation/validateRegistration.js
+++ helpers/validation/validateRegistration.js
@@ -4,9 +4,8 @@
  * @returns {boolean} if valid returns true
  */
 function validateRegistration(registration) {
-	// https://gist.github.com/danielrbradley/7567269
-	const regex =
-		/(^[A-Z]{2}[0-9]{2}\s?[A-Z]{3}$)|(^[A-Z][0-9]{1,3}[A-Z]{3}$)|(^[A-Z]{3}[0-9]{1,3}[A-Z]$)|(^[0-9]{1,4}[A-Z]{1,2}$)|(^[0-9]{1,3}[A-Z]{1,3}$)|(^[A-Z]{1,2}[0-9]{1,4}$)|(^[A-Z]{1,3}[0-9]{1,3}$)|(^[A-Z]{1,3}[0-9]{1,4}$)/i;
+	// Standard UK + NI + Cherished format. Based on https://gist.github.com/danielrbradley/7567269
+	const regex = /(^[A-Z]{1,3}[0-9]{1,4}[A-Z]{0,3}$)|(^[0-9]{1,4}[A-Z]{1,3}$)/i;
 	return regex.test(registration);
 }
 
