75e1b18af418df23dd7d40294b3eb457a462ecea
Will K <45063456+williamxb@users.noreply.github.com>
2026-07-13 09:59:58 +0100

    fix: length check removal regex checks for length when validating formats anyway

--- helpers/validation/validateRegistration.js
+++ helpers/validation/validateRegistration.js
@@ -4,6 +4,7 @@
  * @returns {boolean} if valid returns true
  */
 function validateRegistration(registration) {
+	if (registration.length > 7) return false;
 	// Standard UK + NI + Cherished format. Based on https://gist.github.com/danielrbradley/7567269
 	const regex = /(^[A-Z]{1,3}[0-9]{1,4}[A-Z]{0,3}$)|(^[0-9]{1,4}[A-Z]{1,3}$)/i;
 	return regex.test(registration);
